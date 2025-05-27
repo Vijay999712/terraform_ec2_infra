de931938d1215fc171d254b6fd6cc10e527f2aa4
Vijay999712 <vijaybabu920@gmail.com>
2025-05-27 14:39:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,33 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0e58b56aa4d64231b"  # Amazon Linux 2 AMI in us-east-1
   instance_type = "t2.medium"
-  key_name      = "east1"             # 🔑 Replace with your actual key pair name
+  key_name      = "east1"             # Replace with your actual key pair name
+
+  user_data = <<-EOF
+    #!/bin/bash
+    set -e
+
+    yum update -y
+    yum install -y git
+    amazon-linux-extras install docker -y
+    systemctl start docker
+    systemctl enable docker
+    usermod -aG docker ec2-user
+
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    chmod +x kubectl
+    mv kubectl /usr/local/bin/
+    chown ec2-user:ec2-user /usr/local/bin/kubectl
+
+    yum install -y conntrack
+
+    curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    chmod +x minikube
+    mv minikube /usr/local/bin/
+    chown ec2-user:ec2-user /usr/local/bin/minikube
+
+    grep -qxF 'export PATH=$PATH:/usr/local/bin' /home/ec2-user/.bash_profile || echo 'export PATH=$PATH:/usr/local/bin' >> /home/ec2-user/.bash_profile
+  EOF
 
   tags = {
     Name = "Terraform-EC2-t2-medium"
