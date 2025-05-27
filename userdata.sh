e87b3d5fcca04e3df9c5c29c2a826c31a40eb8bb
Vijay999712 <vijaybabu920@gmail.com>
2025-05-27 15:43:10 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,6 +3,9 @@ set -xe
 
 yum update -y
 
+# install git
+yum install git -y
+
 # Install Docker on Amazon Linux 2
 amazon-linux-extras enable docker
 yum install -y docker
@@ -26,6 +29,8 @@ curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/miniku
 chmod +x minikube
 mv minikube /usr/local/bin/
 chown ec2-user:ec2-user /usr/local/bin/minikube
+# Start minikube as ec2-user using none driver (bare metal)
+sudo -i -u ec2-user bash -c 'minikube start --driver=none'
 
 # Ensure /usr/local/bin is in ec2-user's PATH
 grep -qxF 'export PATH=$PATH:/usr/local/bin' /home/ec2-user/.bash_profile || echo 'export PATH=$PATH:/usr/local/bin' >> /home/ec2-user/.bash_profile
