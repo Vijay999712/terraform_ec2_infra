ddce2a80ef1f38172889e82607a7dc9b686119af
Vijay999712 <vijaybabu920@gmail.com>
2025-05-27 15:26:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,50 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0e58b56aa4d64231b"  # Amazon Linux 2 AMI in us-east-1
-  instance_type = "t2.medium"
-  key_name      = "east1"                   # Replace with your actual key pair name
-  user_data = file("userdata.sh")
+resource "aws_security_group" "minikube_sg" {
+  name        = "minikube-sg"
+  description = "Allow NodePort and web access"
 
+  ingress {
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow NodePort
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Optional for HTTP apps
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Optional for HTTPS apps
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "minikube_ec2" {
+  ami                    = "ami-0e58b56aa4d64231b" # Ubuntu 24.04 LTS (verify latest)
+  instance_type          = "t2.medium"
+  key_name               = "east1"
+  security_groups        = [aws_security_group.minikube_sg.name]
+  associate_public_ip_address = true
+
+  user_data              = file("userdata.sh")
 
   tags = {
-    Name = "Terraform-EC2-t2-medium"
+    Name = "MinikubeEC2"
   }
 }
